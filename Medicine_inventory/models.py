5f5171352cb730fe17e9086d95aa2bb350178efc
Yoosuf Ahamed <yahamed95@gmail.com>
2025-07-18 23:02:06 +0530

    added column to medicine model

--- Medicine_inventory/models.py
+++ Medicine_inventory/models.py
@@ -8,13 +8,10 @@ class Medicine(models.Model):
     description = models.TextField(blank=True)
     dosage = models.CharField(max_length=50)  # e.g., 500mg
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    
     quantity_in_stock = models.PositiveIntegerField()
     reorder_level = models.PositiveIntegerField(default=10)  # Minimum quantity before restock alert
-
     manufacture_date = models.DateField()
     expiry_date = models.DateField()
-    
     batch_number = models.CharField(max_length=50, unique=True)
     supplier = models.CharField(max_length=100)  # You can replace this with a ForeignKey to Supplier model
 
