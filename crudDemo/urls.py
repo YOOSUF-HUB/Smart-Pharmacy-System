526168cea4802c1fbe55cb5a83d46ad861eee085
Yoosuf Ahamed <yahamed95@gmail.com>
2025-07-23 02:26:28 +0530

    Refactor URL routing to set the root URL to the medicine card view

--- crudDemo/urls.py
+++ crudDemo/urls.py
@@ -5,13 +5,8 @@ from crudApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Student app URLs
-    path('view/', views.student_view, name='student_view'),
-    path('create', views.create_student, name='student_create'),
-    path('delete/<id>', views.delete_student),
-    path('update/<id>', views.update_student),
-    path('', views.home, name='home'),
 
-    # Medicine_inventory app URLs
-    path('medicine/', include('Medicine_inventory.urls')),  # include medicine app URLs here
+    # Set the root URL to the medicine card view
+    path('', include('Medicine_inventory.urls')),  # now root goes to medicine app
+
 ]
