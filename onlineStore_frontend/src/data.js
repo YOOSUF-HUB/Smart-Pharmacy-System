ece438a4a06579474506c228aca696e7815b8818
saabikweb <mohammedshaabik@gmail.com>
2025-08-28 09:22:39 +0530

    online store 4

---
+++ onlineStore_frontend/src/data.js
@@ -0,0 +1,51 @@
+const medicine = [
+  {
+    _id: "1",
+    name: "Panadol",
+    image:
+      "https://images.pexels.com/photos/159211/headache-pain-pills-medication-159211.jpeg",
+    brand: "Panadol",
+    category: "Pain Relief",
+    description: "Effective relief for headaches and minor aches.",
+    price: 160,
+  },
+  {
+    _id: "2",
+    name: "Amoxicillin",
+    image:
+      "https://images.pexels.com/photos/159211/headache-pain-pills-medication-159211.jpeg",
+    brand: "Amoxicillin",
+    category: "Antibiotic",
+    description: "Broad-spectrum antibiotic for bacterial infections.",
+    price: 200,
+  },
+  {
+    _id: "3",
+    name: "Ibuprofen",
+    image:
+      "https://images.pexels.com/photos/159211/headache-pain-pills-medication-159211.jpeg",
+    brand: "Ibuprofen",
+    category: "Pain Relief",
+    description: "Nonsteroidal anti-inflammatory drug.",
+    price: 180,
+  },
+  {
+    _id: "4",
+    name: "Paracetamol",
+    image:
+      "https://images.pexels.com/photos/159211/headache-pain-pills-medication-159211.jpeg",
+    brand: "Paracetamol",
+    category: "Pain Relief",
+    description: "Common pain reliever and fever reducer.",
+    price: 150,
+  }
+];
+
+// const category = [
+//   { _id: 0, name: "ALL" },
+//   { _id: 1, name: "Rx" },
+//   { _id: 2, name: "Non Rx" },
+// ];
+const category = ["Pain Relief", "Allergy", "Digestive", "Cold & Flu", "Vitamins"];
+
+export { medicine, category };
